53fcc6344336de4f34a7b5cbbf9daff1d44fbee3
Ouissam Gouni <g.ouissam@gmail.com>
2026-05-02 12:06:32 +0200

    refactor: update CORS middleware to allow PUT method in API requests

--- backend/api/main.py
+++ backend/api/main.py
@@ -96,7 +96,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=get_cors_origins(),
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization"],
 )
 
