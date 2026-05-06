46793b9cd530ca8317b9f603edbca8e47743aabe
Ouissam Gouni <g.ouissam@gmail.com>
2026-05-06 02:01:48 +0200

    feat: refactor TagFilterPills component to streamline rendering of tags and improve layout

--- frontend/src/components/TagFilterPills.tsx
+++ frontend/src/components/TagFilterPills.tsx
@@ -85,57 +85,36 @@ export default function TagFilterPills({ tagGroups, activeTagIds, onToggle, onCl
         );
     };
 
-    return (
-        <div className="flex flex-col gap-1">
-            {/* Hero row */}
-            {heroTags.length > 0 && (
-                <div className="flex flex-wrap gap-1 items-center">
-                    {activeTagIds.size > 0 && heroTags.some((t) => activeTagIds.has(t.id)) && (
-                        <button
-                            onClick={onClear}
-                            className="inline-flex h-5 w-5 items-center justify-center text-rose-500 hover:text-rose-700 font-semibold mr-1"
-                            aria-label="Clear tag filters"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3.5 w-3.5">
-                                <path fillRule="evenodd" d="M4.22 4.22a.75.75 0 0 1 1.06 0L10 8.94l4.72-4.72a.75.75 0 1 1 1.06 1.06L11.06 10l4.72 4.72a.75.75 0 1 1-1.06 1.06L10 11.06l-4.72 4.72a.75.75 0 0 1-1.06-1.06L8.94 10 4.22 5.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                    )}
-                    {heroTags.map(renderPill)}
-                </div>
-            )}
+    const allVisible = [...heroTags, ...visibleRemainder];
 
-            {/* Remainder row */}
-            {remainderTags.length > 0 && (
-                <div className="flex flex-wrap gap-1 items-center">
-                    {heroTags.length === 0 && activeTagIds.size > 0 && (
-                        <button
-                            onClick={onClear}
-                            className="inline-flex h-5 w-5 items-center justify-center text-rose-500 hover:text-rose-700 font-semibold mr-1"
-                            aria-label="Clear tag filters"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3.5 w-3.5">
-                                <path fillRule="evenodd" d="M4.22 4.22a.75.75 0 0 1 1.06 0L10 8.94l4.72-4.72a.75.75 0 1 1 1.06 1.06L11.06 10l4.72 4.72a.75.75 0 1 1-1.06 1.06L10 11.06l-4.72 4.72a.75.75 0 0 1-1.06-1.06L8.94 10 4.22 5.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                    )}
-                    {visibleRemainder.map(renderPill)}
-                    {hasMore && (
-                        <button
-                            onClick={() => setExpanded((v) => !v)}
-                            className="inline-flex items-center justify-center h-5 w-5 rounded-full border border-gray-300 text-gray-400 hover:border-gray-400 hover:text-gray-600 transition"
-                            aria-label={expanded ? 'Show fewer tags' : 'Show more tags'}
-                            title={expanded ? 'Show fewer' : `Show ${remainderTags.length - MAX_VISIBLE} more`}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3 w-3">
-                                {expanded
-                                    ? <path fillRule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clipRule="evenodd" />
-                                    : <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                                }
-                            </svg>
-                        </button>
-                    )}
-                </div>
+    return (
+        <div className="flex flex-wrap gap-1 items-center">
+            {activeTagIds.size > 0 && (
+                <button
+                    onClick={onClear}
+                    className="inline-flex h-5 w-5 items-center justify-center text-rose-500 hover:text-rose-700 font-semibold mr-1"
+                    aria-label="Clear tag filters"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3.5 w-3.5">
+                        <path fillRule="evenodd" d="M4.22 4.22a.75.75 0 0 1 1.06 0L10 8.94l4.72-4.72a.75.75 0 1 1 1.06 1.06L11.06 10l4.72 4.72a.75.75 0 1 1-1.06 1.06L10 11.06l-4.72 4.72a.75.75 0 0 1-1.06-1.06L8.94 10 4.22 5.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                    </svg>
+                </button>
+            )}
+            {allVisible.map(renderPill)}
+            {hasMore && (
+                <button
+                    onClick={() => setExpanded((v) => !v)}
+                    className="inline-flex items-center justify-center h-5 w-5 rounded-full border border-gray-300 text-gray-400 hover:border-gray-400 hover:text-gray-600 transition"
+                    aria-label={expanded ? 'Show fewer tags' : 'Show more tags'}
+                    title={expanded ? 'Show fewer' : `Show ${remainderTags.length - MAX_VISIBLE} more`}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3 w-3">
+                        {expanded
+                            ? <path fillRule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clipRule="evenodd" />
+                            : <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                        }
+                    </svg>
+                </button>
             )}
         </div>
     );
